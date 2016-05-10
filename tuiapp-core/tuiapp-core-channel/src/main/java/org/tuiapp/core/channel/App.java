29564500eb387a4fa61c7895c67ddf02aeb2d681
bober.hu <bober.hu@outlook.com>
2016-05-10 16:41:55 +0800

    first commit
    Signed-off-by: bober.hu <bober.hu@outlook.com>

---
+++ tuiapp-core/tuiapp-core-channel/src/main/java/org/tuiapp/core/channel/App.java
@@ -0,0 +1,13 @@
+package org.tuiapp.core.channel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ tuiapp-core/tuiapp-core-user/src/main/java/org/tuiapp/core/user/App.java
@@ -0,0 +1,13 @@
+package org.tuiapp.core.user;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
